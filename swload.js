6a9b7df4c6870cd7520ee6c6110daf08f4146914
Джексон Миссиссиппи <tripwiregamer@gmail.com>
2024-05-14 15:12:36 -0500

    Update swload.js

--- swload.js
+++ swload.js
@@ -1,14 +1,4 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js', { scope: '/' })
-        .then((registration) => {
-            const data = {
-                type: 'CACHE_URLS',
-                payload: [
-                    location.href,
-                    ...performance.getEntriesByType('resource').map((r) => r.name)
-                ]
-            };
-            registration.installing.postMessage(data);
-        })
-        .catch((err) => console.log('SW registration FAIL:', err));
+    window.addEventListener('load', () =>
+        navigator.serviceWorker.register('/sw.js'));
 }
