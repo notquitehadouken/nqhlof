de736ede3b1e40b458e08519078e9ceabfde5ebb
Джексон Миссиссиппи <tripwiregamer@gmail.com>
2024-05-14 15:17:08 -0500

    yes.

--- sw.js
+++ sw.js
@@ -1,18 +1,29 @@
 // No rights reserved.
 
-const name = "SW-NQH";
+const cacheName = "SW-CACHE";
 
 self.addEventListener('install', (e) => {
-    e.waitUntil(self.skipWaiting());
+    e.waitUntil(
+        caches
+        .open(cacheName)
+        .then(cache => cache.addAll("/"))
+        .then(() => self.skipWaiting()));
 });
 
 self.addEventListener('message', (e) => {
-    if (e.data.type === 'CACHE_URLS') {
-        e.waitUntil(
-            caches.open(name)
-                .then( (cache) => {
-                    return cache.addAll(event.data.payload);
+    e.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== cacheName) {
+                        return caches.delete(cache);
+                    }
                 })
-        );
-    }
+            )
+        })
+    )
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)))l
 });
