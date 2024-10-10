388ebcd5501ee030aaa8e83f5fde1dcc7558f6e5
notquitehadouken <tripwiregamer@gmail.com>
2024-10-10 11:14:46 -0500

    Index done

--- js/splitter.js
+++ js/splitter.js
@@ -1,14 +1,14 @@
-document.querySelectorAll("[textoscillator]").forEach((elem) => {
+document.querySelectorAll("[textsplitter]").forEach((elem) => {
     let val = elem.innerHTML;
     elem.innerHTML = "";
     for (let i = 0; i < val.length; i++) {
-        let spanElem = document.createElement("span");
+        let spanElem = document.createElement("pre");
         spanElem.innerHTML = val[i];
-        let off = document.currentScript.getAttribute("oscillatorOffset");
+        let off = document.currentScript.getAttribute("offset");
         if (!off)
             off = 0.15;
         spanElem.style.setProperty("animation-delay", (off * i).toString() + "s");
-        spanElem.className = "textoscillator";
+        spanElem.className = "textsplitter";
         elem.appendChild(spanElem);
     }
 })
