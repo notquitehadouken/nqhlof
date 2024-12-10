c5aefaa07cba80b9aba4f6b717c58eb123644440
Джексон Миссиссиппи <tripwiregamer@gmail.com>
2024-12-10 08:37:43 -0600

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,16 +1,23 @@
 let img = document.getElementById("theImage");
+let clean = (showEl)=>{Array.from(document.getElementById("imageSelector").children).forEach((x) => {if(!showEl.includes(x))x.className = "hide"});}
 setInterval(function(){
-    Array.from(document.getElementById("imageSelector").children).forEach((x) => x.className = "hide");
+    let showEl = [];
     let oldEl = document.getElementById("start");
     let currEl = oldEl
     let currName = "";
     do {
         oldEl = currEl;
-        document.getElementById(currName+"L").className = currEl.className = "";
-        if (currEl.value.length === 0)
+        let label = document.getElementById(currName+"L");
+        label.className = currEl.className = "";
+        showEl.push(currEl);
+        showEl.push(label);
+        if (currEl.value.length === 0) {
+            clean(showEl);
             return (img.src = "");
+        }
         currName = currEl.value + "_" + currName;
     } while ((currEl = document.getElementById(currName)));
     if (img.src !== oldEl.value)
         img.src = oldEl.value;
+    clean(showEl);
 },30);
