076ed604c23b8cd5e068952dc71d241f43bac501
notquitehadouken <tripwiregamer@gmail.com>
2026-04-24 11:18:38 -0500

    die

--- files/os0.lua
+++ files/os0.lua
@@ -5,17 +5,31 @@ gpu.bind(screenaddr)
 gpu.setViewport(32, 16)
 gpu.setResolution(32, 16)
 
-idx = 0
-max = 256 * 256 * 256
-while os.sleep(10) do
-	for x = 0, 31 do
-		for y = 0, 15 do
-			gpu.set(x + 1, y + 1, "#")
-      idx += 1
-      gpu.setForeground(idx)
-		end
-	end
-  if idx >= max then
-    idx = 0
+blueshades = {0x00, 0x40, 0x80, 0xC0, 0xFF}
+rgshades = {0x00, 0x33, 0x66, 0x99, 0xCC, 0xFF}
+bwshades = {0x0F, 0x1E, 0x2D, 0x3C, 0x4B, 0x5A, 0x69, 0x78, 0x87, 0x96, 0xA5, 0xB4, 0xC3, 0xD2, 0xE1, 0xF0}
+
+colorof = function(index)
+  if index >= 240 then
+    index = index - 240
+    local shade = bwshades[index]
+    local color = shade + shade * 256 + shade * 65536
+    return color
+  end
+  local blue = index % 5
+  local green = math.floor(index / 5) % 8
+  local red = math.floor(index / 40)
+  return red * 65536 + green * 256 + blue
+end
+
+while 1 do
+  for x = 0, 15 do
+    for y = 0, 15 do
+      idx = y * 16 + x
+      local color = colorof(idx)
+      gpu.setForeground(color)
+      gpu.setBackground(color)
+      gpu.set(x * 2 + 1, y + 1, "##")
+    end
   end
 end
