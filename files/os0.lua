e548cc0d9b18b0c2352fed801160d140e6bb1aac
notquitehadouken <tripwiregamer@gmail.com>
2026-04-24 11:07:29 -0500

    die

--- files/os0.lua
+++ files/os0.lua
@@ -5,11 +5,17 @@ gpu.bind(screenaddr)
 gpu.setViewport(32, 16)
 gpu.setResolution(32, 16)
 
+idx = 0
+max = 256 * 256 * 256
 while os.sleep(10) do
-	for x = 0, 15 do
+	for x = 0, 31 do
 		for y = 0, 15 do
-			str = string.char(y * 16 + x)
-			gpu.set(x + 1, y + 1, str)
+			gpu.set(x + 1, y + 1, "#")
+      idx += 1
+      gpu.setForeground(idx)
 		end
 	end
+  if idx >= max then
+    idx = 0
+  end
 end
