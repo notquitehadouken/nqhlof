3d695cf0983a715283a9a5cd2f41fe817f499175
notquitehadouken <tripwiregamer@gmail.com>
2026-04-24 10:36:54 -0500

    cheese

---
+++ files/os0.lua
@@ -0,0 +1,22 @@
+clist = component.list
+screenaddr = clist("screen")()
+gpu = component.proxy(clist("gpu")())
+gpu.bind(screenaddr)
+gpu.setViewport(32, 16)
+gpu.setResolution(32, 16)
+
+while os.sleep(10) do
+	for x = 0, 15 do
+		for y = 0, 15 do
+			str = string.char(y * 16 + x)
+			gpu.set(x + 1, y + 1, str)
+		end
+	end
+	for x = 0, 15 do
+		for y = 0, 15 do
+			gpu.setBackground(y * 16 + x, true)
+			gpu.setForeground(y * 16 + x, true)
+			gpu.set(x + 17, y + 1, "#")
+		end
+	end
+end
