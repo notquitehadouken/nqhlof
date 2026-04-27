9c6b670ef86ea114c29b3ead3bdb178be02bbe05
notquitehadouken <tripwiregamer@gmail.com>
2026-04-27 14:24:49 -0500

    o

--- files/os0.lua
+++ files/os0.lua
@@ -6,8 +6,8 @@ gpu.setViewport(32, 16)
 gpu.setResolution(32, 16)
 
 blueshades = {0x00, 0x40, 0x80, 0xC0, 0xFF}
-redshades = {0x00, 0x33, 0x66, 0x99, 0xCC, 0xFF}
-greenshades = {0x00, 0x24, 0x49, 0x6D, 0x92, 0xB6, 0xDB, 0xFF}
+redshades = {0x000000, 0x330000, 0x660000, 0x990000, 0xCC0000, 0xFF0000}
+greenshades = {0x0000, 0x2400, 0x4900, 0x6D00, 0x9200, 0xB600, 0xDB00, 0xFF00}
 bwshades = {0x0F, 0x1E, 0x2D, 0x3C, 0x4B, 0x5A, 0x69, 0x78, 0x87, 0x96, 0xA5, 0xB4, 0xC3, 0xD2, 0xE1, 0xF0}
 
 colorof = function(index)
@@ -17,10 +17,10 @@ colorof = function(index)
     local color = shade + shade * 256 + shade * 65536
     return color
   end
-  local blue = index % 5
-  local green = math.floor(index / 5) % 8
-  local red = math.floor(index / 40)
-  return redshades[red] * 65536 + greenshades[green] * 256 + blueshades[blue]
+  local blue = index % 5 + 1
+  local green = math.floor(index / 5) % 8 + 1
+  local red = math.floor(index / 40) + 1
+  return redshades[red] + greenshades[green] + blueshades[blue]
 end
 
 while 1 do
